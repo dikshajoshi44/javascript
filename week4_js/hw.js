e1fa14b21571dc97b4eae66370c8a5ba420d081d
dikshajoshi44 <dikshajoshi131@gmail.com>
2017-12-06 22:25:40 +0530

    js file

---
+++ week4_js/hw.js
@@ -0,0 +1,20 @@
+/*Add the JavaScript here for the function billingFunction().  It is responsible for setting and clearing the fields in Billing Information */
+/*Add the JavaScript here for the function function billingFunction().  It is responsible for setting and clearing the fields in Billing Information */
+function billingFunction(){
+
+		if (same.checked)
+		{
+		var shipName=document.getElementById('shippingName').value;
+		var shipZip=document.getElementById('shippingZip').value;
+
+		document.getElementById('billingName').value = shipName;
+		document.getElementById('billingZip').value = shipZip;
+
+		}
+
+		else
+		{ 
+			document.getElementById('billingName').value = "";
+			document.getElementById('billingZip').value = "";
+		}
+}
